5fee3c91f93226c208abb124cc951e4a21208ce6
MohammadOfficials <mohammadofficial4@gmail.com>
2024-09-26 17:44:49 +0600

    header incomplete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from './shared/Header/Header';
 import Navbar from './shared/Navbar/Navbar';
+import LeftSideNav from './shared/LeftSideNav/LeftSideNav';
+import RightSideNav from './shared/RightSideNav/RightSideNav';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h2 className='text-3xl font-poppins font-bold'>This is Home </h2>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className='md:col-span-2'>
+                    <h2 className='text-4xl'>News coming soon...</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
