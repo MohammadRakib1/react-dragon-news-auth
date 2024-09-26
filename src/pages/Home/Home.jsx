1d0b35c1fc8b7eab89a924ce76ad9f569cfeac3a
MohammadOfficials <mohammadofficial4@gmail.com>
2024-09-26 19:35:59 +0600

    navbar complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from './shared/Header/Header';
-import Navbar from './shared/Navbar/Navbar';
-import LeftSideNav from './shared/LeftSideNav/LeftSideNav';
-import RightSideNav from './shared/RightSideNav/RightSideNav';
+import Header from '../shared/Header/Header';
+import Navbar from '../shared/Navbar/Navbar';
+import LeftSideNav from '../shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../shared/RightSideNav/RightSideNav';
+import BreakingNews from './BreakingNews';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
                 <div>
