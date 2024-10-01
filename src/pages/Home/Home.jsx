2851481bfb4072acda697bf15371d8fe2bf3fc29
MohammadOfficials <mohammadofficial4@gmail.com>
2024-10-01 17:50:10 +0600

    News Load Complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,14 @@ import Navbar from '../shared/Navbar/Navbar';
 import LeftSideNav from '../shared/LeftSideNav/LeftSideNav';
 import RightSideNav from '../shared/RightSideNav/RightSideNav';
 import BreakingNews from './BreakingNews';
+import { useLoaderData } from 'react-router-dom';
+import NewsCard from './NewsCard';
 
 const Home = () => {
+
+    const news = useLoaderData();
+    console.log(news)
+
     return (
         <div>
             <Header></Header>
@@ -16,7 +22,9 @@ const Home = () => {
                     <LeftSideNav></LeftSideNav>
                 </div>
                 <div className='md:col-span-2'>
-                    <h2 className='text-4xl'>News coming soon...</h2>
+                    {
+                        news.map(aNews => <NewsCard key={aNews._id} news={aNews}></NewsCard>)
+                    }
                 </div>
                 <div>
                     <RightSideNav></RightSideNav>
